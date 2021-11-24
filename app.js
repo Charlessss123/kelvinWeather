2de8e031254c7a0ff8853d9822cbbd25ff433cf3
Charlessss123 <91907318+Charlessss123@users.noreply.github.com>
2021-11-24 04:56:42 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let kelvin = 293; //forecast today
+
+
+let celsius = 293 - 273; //kelvin to celsius
+
+let fahrenheit = celsius * (9/5) + 32 //fahrenheit
+
+fahrenheit = Math.floor(fahrenheit); //rounding fahrenheit
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
